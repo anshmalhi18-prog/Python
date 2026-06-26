b9056e423e63ad5e949ef16c3e816c500b3d8692
anshmalhi18-prog <anshmalhi18@gmail.com>
2026-06-26 20:58:02 +1000

    Add files via upload

---
+++ Number_swap.py
@@ -0,0 +1,8 @@
+a=int(input("Please enter your first number"))
+b=int(input("Please enter your second number"))
+c=int(input("Please enter your third number"))
+temp=a
+a=b
+b=c
+c=temp
+print(f"a={a} b={b} c={c}")
